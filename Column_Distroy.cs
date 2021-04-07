a808affb40d6c4ab452c9c08ebc2dffe8a3172af
Tamjiduap <52478397+Tamjiduap@users.noreply.github.com>
2021-04-07 10:01:55 +0600

    Add files via upload

---
+++ Column_Distroy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Column_Distroy : MonoBehaviour
+{
+    public int health = 100;
+    public GameObject deathEfect;
+    public GameObject impactEffect;
+    Human_Movement human;
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    // Update is called once per frame
+    void Die()
+    {
+        Instantiate(deathEfect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+        //human.Human_Move(1);
+    }
+}
